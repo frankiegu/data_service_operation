38325e415e4d6bba6a6b810b6f7055997d6ac942
xiao.luo <568901700@qq.com>
2016-10-27 10:03:37 +0800

    Update settings.py

--- src/DataOperation/DataOperation/settings.py
+++ src/DataOperation/DataOperation/settings.py
@@ -177,14 +177,14 @@ CACHES = {
 
 
 #邮件配置
-EMAIL_HOST = 'smtp.126.com'                   #SMTP地址
+EMAIL_HOST = 'smtp.xxx.com'                   #SMTP地址
 EMAIL_PORT = 25                               #SMTP端口
-EMAIL_HOST_USER = 'zhiaixuexi@126.com'        #我自己的邮箱
-EMAIL_HOST_PASSWORD = 'lx870830'              #我的邮箱密码
+EMAIL_HOST_USER = 'your-email-address'        #我自己的邮箱
+EMAIL_HOST_PASSWORD = 'password'              #我的邮箱密码
 EMAIL_SUBJECT_PREFIX = u'[DC-数据服务运营平台]'     #为邮件Subject-line前缀,默认是'[django]'
 EMAIL_USE_TLS = True								#与SMTP服务器通信时，是否启动TLS链接(安全链接)。默认是false
 #管理员站点
-SERVER_EMAIL = 'zhiaixuexi@126.com'            #The email address that error messages come from, such as those sent to ADMINS and MANAGERS.
+SERVER_EMAIL = 'your-email-address'            #The email address that error messages come from, such as those sent to ADMINS and MANAGERS.
 
 TIME_ZONE = 'Asia/Shanghai'
 #USE_TZ = True
